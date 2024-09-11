2397969e2aa17ff64536ac8b1c2663e86076194c
Mohith <mohithjegan@gmail.com>
2024-09-10 20:29:07 -0400

    New changes are added

---
+++ index.js
@@ -0,0 +1,15 @@
+// alert("hello") Single line comment
+/* Multi line commment
+var name = "John"; // Data Type => STRING
+var age = 50; // Data Type => NUMBER
+var loggedIn = false; // Data Type => BOOLEAN
+var underStatus = null; // Data Type => NULL
+var players; // Data Type => UNDEFINED
+*/
+var thanksMessage = "Thank You ";
+var username = "John";
+var num1 = 8 + 4;
+var num2 = 8 - 4;
+var num3 = 8 * 4;
+var num4 = 8 / 4;
+alert(thanksMessage + username);
